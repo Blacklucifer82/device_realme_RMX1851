ae0d5ba6fafb6a4ed2e507f269da67467c40340f
Roshan Pius <rpius@google.com>
2020-04-24 14:33:28 -0700

    sdm710-common: Add WifiOverlay
    Bug: 148617260
    Test: Device boots up and connects to wifi networks.
    Change-Id: Iec27a6d947a232a42451ef095dc73d1a52327db5

--- device.mk
+++ device.mk
@@ -538,6 +538,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.wifi.supplicant@2.1.vendor \
     WifiOverlay \
     TetheringConfigOverlay \
+    WifiOverlay \    
     wpa_supplicant \
     wpa_supplicant.conf
 
