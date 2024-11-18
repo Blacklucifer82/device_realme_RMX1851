9c39a933d37740591d3107654f4b92a3070c07d9
Blacklucifer82 <sourabhkori82@gmail.com>
2024-11-18 23:55:57 +0530

    RMX1851: pixelage

--- pixelage_RMX1851.mk
+++ pixelage_RMX1851.mk
@@ -4,10 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common LineageOS stuff
+# Inherit some common pixelage stuff
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/pixelage/config/common_full_phone.mk)
 
 # Inherit from RMX1851 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -15,15 +15,15 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := RMX1851
 PRODUCT_MANUFACTURER := realme
-PRODUCT_NAME := lineage_RMX1851
+PRODUCT_NAME := pixelage_RMX1851
 PRODUCT_MODEL := RMX1851
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    RisingChipset=sdm710 \
-    RisingMaintainer=Rishi
-
-RISING_MAINTAINER=Rishi
 TARGET_ENABLE_BLUR :=false
+
+# Project Pixelage Flags
+TARGET_FACE_UNLOCK_SUPPORTED := true
+PIXELAGE_BUILDTYPE := UNOFFICIAL
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
