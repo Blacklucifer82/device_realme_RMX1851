1b3b20d8418a7d3e31200338cdb00e481dc0ae76
Blacklucifer82 <sourabhkori82@gmail.com>
2024-11-21 07:58:22 +0530

    RMX1851: Bringup for Pixelage

--- pixelage_RMX1851.mk
+++ pixelage_RMX1851.mk
@@ -4,10 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common LineageOS stuff
+# Inherit some common Pixelage stuff
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+PIXELAGE_BUILDTYPE := UNOFFICIAL
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
+$(call inherit-product, vendor/pixelage/config/common_full_phone.mk)
 
 # Inherit from RMX1851 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -15,7 +18,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := RMX1851
 PRODUCT_MANUFACTURER := realme
-PRODUCT_NAME := lineage_RMX1851
+PRODUCT_NAME := pixelage_RMX1851
 PRODUCT_MODEL := RMX1851
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
