93f31218a0f6b9cbfe0519fb17655d6e19293869
Nolen Johnson <johnsonnolen@gmail.com>
2025-03-10 15:36:30 -0400

    RMX1851: Migrate away from TARGET_RECOVERY_DEVICE_MODULES
    * Dead as of QPR2.
    
    Change-Id: I05bcf246266b89fa7da964f7aa75870b5a63a3b9
    Signed-off-by: Ij Israfil <israfilkhan494@gmail.com>
    Signed-off-by: kaderbava <ksbava7325@gmail.com>

--- device.mk
+++ device.mk
@@ -424,6 +424,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     RemovePackages
 
+# Recovery
+PRODUCT_PACKAGES += \
+    libinit_RMX1851.recovery
+
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
