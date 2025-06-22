c179f6a259e2b367deee0061de6919304b664bb7
Blacklucifer82 <sourabhkori82@gmail.com>
2025-06-22 09:08:13 +0530

    Revert "RMX1851: Adopt new rum"
    This reverts commit cf1da69f504b32eb1018223f2a75cea5967506bb.

--- device.mk
+++ device.mk
@@ -543,5 +543,3 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
-
--include vendor/lineage-priv/keys/keys.mk
