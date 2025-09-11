73b6e9f6bd326730cfd85b5013f9e099b3406b09
Blacklucifer82 <sourabhkori82@gmail.com>
2025-09-11 14:43:06 +0530

    RMX1851: Bringup !!! @ProjectInfinity-X

--- infinity_RMX1851.mk
+++ infinity_RMX1851.mk
@@ -8,7 +8,9 @@
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_ENABLE_BLUR :=false
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+INFINITY_MAINTAINER := Sourabh_X_DiveshKumar
+WITH_GAPPS := true
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
 # Inherit from RMX1851 device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -16,7 +18,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := RMX1851
 PRODUCT_MANUFACTURER := realme
-PRODUCT_NAME := lineage_RMX1851
+PRODUCT_NAME := infinity_RMX1851
 PRODUCT_MODEL := RMX1851
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
