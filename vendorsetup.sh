953a8e6a616dd3c7963b128e6380029f679d4d23
Blacklucifer82 <sourabhkori82@gmail.com>
2026-01-08 20:10:25 +0530

    RMX1851: change kernel
    updated & change default kernel to EOL

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -17,7 +17,7 @@ banner 2>1
 banner 2>1
 echo "Cloning Kernel..."
 git clone --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/clang-proton
-git clone --depth=1 https://github.com/lazycodebuilder/lazy_kernel_realme_sdm710 -b bloodmoon kernel/realme/sdm710
+git clone --depth=1 https://github.com/Blacklucifer82/android_kernel_realme_sdm710.git -b 16 kernel/realme/sdm710
 
 # vendor
 banner 2>1
